b73388fd9da613797d60310457ff222c77e556f5
robalexg <alexg7070@yahoo.com>
2022-11-16 07:48:03 -0600

    set up and homepage

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+
+const Home = () => {
+    return(
+       <section className='home--container'>
+            <div className='home--bg'>
+                
+            </div>
+       </section>
+    )
+}
+
+export default Home
